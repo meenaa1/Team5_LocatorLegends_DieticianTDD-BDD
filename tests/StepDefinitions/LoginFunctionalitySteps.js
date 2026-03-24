09b72e73c35461bcae209ae51ccf3b5cea2fc515
meenaa1 <meenaa.kannan1@outlook.com>
2026-03-23 21:01:34 -0700

    Completed Login page Functional Verification Feature Stepdefinitions

--- tests/StepDefinitions/LoginFunctionalitySteps.js
+++ tests/StepDefinitions/LoginFunctionalitySteps.js
@@ -1,68 +1,67 @@
 import { createBdd } from "playwright-bdd";
-const{Given,When, Then} = createBdd();
+import { test } from "../Fixtures/testFixtures.js"; 
 
-Given('User is on the login page', async ({}) => {
-  // Step: Given User is on the login page
-  // From: tests\Features\LoginPageFunctionalVerification.feature:4:5
+
+const { Given, When, Then } = createBdd(test);
+
+Given('User is on the login page', async ({ loginPage }) => {
+  await loginPage.navigate();
 });
 
-When('User clicks login button after entering valid credentials', async ({}) => {
-  // Step: When User clicks login button after entering valid credentials
-  // From: tests\Features\LoginPageFunctionalVerification.feature:7:5
+When('User clicks login button after entering valid credentials', async ({ loginPage }) => {
+  await loginPage.loginValidUser();
 });
 
-Then('User should be redirected to dashboard page', async ({}) => {
-  // Step: Then User should be redirected to dashboard page
-  // From: tests\Features\LoginPageFunctionalVerification.feature:8:5
+Then('User should be redirected to dashboard page', async ({ loginPage }) => {
+  await loginPage.expectDashboardVisible();
+   logger.info('User is in Dashboard')
 });
 
-When('User clicks login button after entering non existing user', async ({}) => {
-  // Step: When User clicks login button after entering non existing user
-  // From: tests\Features\LoginPageFunctionalVerification.feature:11:5
+When('User clicks login button after entering non existing user', async ({ loginPage }) => {
+  await loginPage.loginNonExistingUser();
+   logger.info('User clicked the login button with Non existing user credentials')
 });
 
-Then('An error message {string} should be displayed', async ({}, arg) => {
-  // Step: Then An error message "Invalid username or password" should be displayed
-  // From: tests\Features\LoginPageFunctionalVerification.feature:12:5
+Then('An error message {string} should be displayed', async ({ loginPage }, expectedError) => {
+  await loginPage.expectErrorMsg(expectedError);
+  logger.info('Error Message Displayed')
 });
 
-When('User clicks login button after entering spl charac in username', async ({}) => {
-  // Step: When User clicks login button after entering spl charac in username
-  // From: tests\Features\LoginPageFunctionalVerification.feature:15:5
+When('User clicks login button after entering spl charac in username', async ({ loginPage }) => {
+  await loginPage.loginSpecialCharUsername();
+  logger.info('User entered special Characters in Username')
 });
 
-When('User clicks login button after entering only few charac of username', async ({}) => {
-  // Step: When User clicks login button after entering only few charac of username
-  // From: tests\Features\LoginPageFunctionalVerification.feature:19:5
+When('User clicks login button after entering only few charac of username', async ({ loginPage }) => {
+  await loginPage.loginShortUsername();
+  logger.info('User entered only few character of username')
 });
 
-When('User clicks login button after entering wrong password', async ({}) => {
-  // Step: When User clicks login button after entering wrong password
-  // From: tests\Features\LoginPageFunctionalVerification.feature:23:5
+When('User clicks login button after entering wrong password', async ({ loginPage }) => {
+  await loginPage.loginWrongPassword();
 });
 
-When('User clicks login button after entering spl charach in password', async ({}) => {
-  // Step: When User clicks login button after entering spl charach in password
-  // From: tests\Features\LoginPageFunctionalVerification.feature:27:5
+When('User clicks login button after entering spl charach in password', async ({ loginPage }) => {
+  await loginPage.loginSpecialCharPassword();
+  logger.info('User entered special Characters in Password')
 });
 
-When('User clicks login button after entering only password', async ({}) => {
-  // Step: When User clicks login button after entering only password
-  // From: tests\Features\LoginPageFunctionalVerification.feature:31:5
+When('User clicks login button after entering only password', async ({ loginPage }) => {
+  await loginPage.loginEmptyUsername();
+  logger.info('Only Password entered')
 });
 
-Then('An error message Username is Required should be displayed', async ({}) => {
-  // Step: Then An error message Username is Required should be displayed
-  // From: tests\Features\LoginPageFunctionalVerification.feature:32:5
+Then('An error message Username is Required should be displayed', async ({ loginPage }) => {
+  await loginPage.expectErrorEmptyUsername();
+  logger.info('Username required- Error message displayed ')
 });
 
-
-When('User clicks login button after entering only username', async ({}) => {
-  // Step: When User clicks login button after entering only username
-  // From: tests\Features\LoginPageFunctionalVerification.feature:35:5
+When('User clicks login button after entering only username', async ({ loginPage }) => {
+  await loginPage.loginEmptyPassword();
+  logger.info('Only Username entered')
 });
 
-Then('An error message Password is Required should be displayed', async ({}) => {
-  // Step: Then An error message Password is Required should be displayed
-  // From: tests\Features\LoginPageFunctionalVerification.feature:36:5
+Then('An error message Password is Required should be displayed', async ({ loginPage }) => {
+  await loginPage.expectErrorEmptyPassword();
+  logger.info('Password required- Error message displayed ')
 });
